20912b181fed24aacdf0a9b1aacaac822b7d536d
rofbol <Flaviu_Boldea@waters.com>
2022-03-31 21:41:16 +0300

    Add test to check build actions

--- BestWorkshop.SearchCitiesTests/SearchCitiesFeature.cs
+++ BestWorkshop.SearchCitiesTests/SearchCitiesFeature.cs
@@ -1,6 +1,13 @@
+using Xunit;
+
 namespace BestWorkshop.SearchCitiesTests
 {
     public class SearchCitiesFeature
     {
+        [Fact]
+        public void TestBuildActions()
+        {
+            Assert.Equal(1, 1);
+        }
     }
 }
