8cb191bcacf726f6ee95a3b97c435317b450afd4
rofbol <Flaviu_Boldea@waters.com>
2022-03-31 21:51:57 +0300

    Remove build test

--- BestWorkshop.SearchCitiesTests/SearchCitiesFeature.cs
+++ BestWorkshop.SearchCitiesTests/SearchCitiesFeature.cs
@@ -4,10 +4,6 @@ namespace BestWorkshop.SearchCitiesTests
 {
     public class SearchCitiesFeature
     {
-        [Fact]
-        public void TestBuildActions()
-        {
-            Assert.Equal(1, 1);
-        }
+        
     }
 }
