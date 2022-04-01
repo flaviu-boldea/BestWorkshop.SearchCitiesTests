edf8f6d324099ed8c118686eab52f965f51085d6
rofbol <Flaviu_Boldea@waters.com>
2022-04-01 11:48:46 +0300

    Add test for cities starting with

--- BestWorkshop.SearchCitiesTests/SearchCitiesFeature.cs
+++ BestWorkshop.SearchCitiesTests/SearchCitiesFeature.cs
@@ -4,6 +4,14 @@ namespace BestWorkshop.SearchCitiesTests
 {
     public class SearchCitiesFeature
     {
-        
+        [Theory]
+        [InlineData("Va", new[] {"Valencia", "Vancouver"})]
+        [InlineData("Val", new[] {"Valencia"})]
+        public void GivenAListOfCities_WhenSearch_ThenReturnsCitiesStartingWithSearchText(string searchText, string[] output)
+        {
+
+        }
+
+
     }
 }
